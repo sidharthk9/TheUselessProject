15ee031249aaaa3c8059235a832de665e6010510
Sidharth K <sidharth.k9@mailbox.org>
2020-11-08 10:33:32 +0400

    Added the Log-In page.

---
+++ src/Shared/Auth/LogIn.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {Card, Image, Form, Button, Segment} from "semantic-ui-react";
+// Components
+import "../../assets/semantic/dist/semantic.min.css";
+import logo from "../../Static/Images/logo.jpeg"
+
+
+const LogIn = () => {
+    return(
+        <Card.Group itemsPerRow={1}>
+
+            <Card color={ "purple" }>
+                <Image
+                    size="small"
+                    centered
+                    wrapped
+                    ui={ false }
+                    src={ logo }
+                />
+                <Card.Content textAlign={ "center" }>
+                    <Card.Header>BT RAK</Card.Header>
+                    <Card.Meta>Welcome Back!</Card.Meta>
+                </Card.Content>
+            </Card>
+
+            <Card color={ "violet" }>
+
+                <Card.Content textAlign={ "left" }>
+                    <Card.Header>Log In</Card.Header>
+                </Card.Content>
+
+                <Card.Content extra textAlign={ "center" }>
+                    <Segment inverted>
+                        <Form inverted>
+                            <Form.Group widths="equal">
+                                <Form.Input fluid label="Email" />
+                                <Form.Input fluid label="Password" />
+                            </Form.Group>
+                            <Form.Checkbox label="Stay Logged In" />
+                            <Button
+                                content="Log In"
+                                color="blue"
+                                size="large"
+                            />
+                        </Form>
+                    </Segment>
+                </Card.Content>
+
+            </Card>
+
+        </Card.Group>
+    );
+}
+
+export default LogIn;
